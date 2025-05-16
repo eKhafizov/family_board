91849d8741be8b6ec2013c38d23005f15752ef20
eKhafizov <130173250+eKhafizov@users.noreply.github.com>
2025-05-16 18:23:25 +0200

    Добавлен endpoint topUpFamily и хук useTopUpFamilyMutation

--- app/main.py
+++ app/main.py
@@ -1,24 +1,4 @@
-# from fastapi import FastAPI
-# from fastapi.middleware.cors import CORSMiddleware
-# from app.database import engine, Base
-# from app.routers import users, families, tasks
 
-# app = FastAPI()
-# # при старте создаём все таблицы, если ещё нет
-# Base.metadata.create_all(bind=engine)
-
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],       # в проде ограничьте до своих доменов
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
-# app.include_router(users.router, prefix="/users/users", tags=["users"])
-# app.include_router(families.router, prefix="/families", tags=["families"])
-# app.include_router(tasks.router, prefix="/tasks", tags=["tasks"])
-# app/main.py
 import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
